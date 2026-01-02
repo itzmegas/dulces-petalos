8fa1433e9b2edb4275f6cb0cb1a0ea56b2357fb6
itzmegas <megasgnr@gmail.com>
2026-01-02 14:45:55 +0100

    modified layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,12 @@
 import type { PropsWithChildren } from "react";
+import { Header } from "./Header";
 import classes from "./Layout.module.css";
 
 export const Layout = ({ children }: PropsWithChildren) => {
-	return <div className={classes.layout}>{children}</div>;
+	return (
+		<div className={classes.layout}>
+			<Header />
+			<div className={classes.body}>{children}</div>
+		</div>
+	);
 };
